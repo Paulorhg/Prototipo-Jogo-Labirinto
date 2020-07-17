6515eacc4876c166442ef797661e77821ebd4527
Paulo Ricardo Horta Grando <paulorhg@hotmail.com>
2020-07-17 15:05:52 -0300

    primeiro commit

---
+++ Movimento em 3a pessoa/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)){
+
+            StartCoroutine("Attack");
+            
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+
+            anim.SetBool("defend", true);
+
+        }
+        else
+            anim.SetBool("defend", false);
+
+    }
+
+
+    IEnumerator Attack()
+    {
+        if (anim.GetBool("walk"))
+            anim.SetBool("walk", false);
+        anim.SetBool("attack", true);
+        yield return new WaitForSeconds(.7f);
+        anim.SetBool("attack", false);
+    }
+
+
+}
---
+++ Movimento em 3a pessoa/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    private Animator anim;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVeloity;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVeloity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            if(!anim.GetBool("attack"))
+                anim.SetBool("walk", true);
+        }
+        else
+            anim.SetBool("walk", false);
+
+    }
+}
